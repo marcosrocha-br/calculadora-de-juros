c427ffca5188caa95755a51281b50fb7c4f96fbf
Marcos Rocha <marcosrocha.br@hotmail.com>
2020-05-14 16:04:55 -0300

    Organização de id e classes,  V2 do css

--- js/juros-simples.js
+++ js/juros-simples.js
@@ -5,8 +5,6 @@ calc.addEventListener('click',calcular)
 var limp = document.getElementById('limpar')
 limp.addEventListener('click', limpar)
 
-
-     
             //Função calcular
 function calcular(){
             //Campos
@@ -14,32 +12,29 @@ function calcular(){
     var taxa = document.getElementById('taxa')
     var tempo = document.getElementById('tempo')
 
-                //Conversão
+            //Conversão
     var c = Number(capital.value)
     var i = Number(taxa.value)
     var t = Number(tempo.value)
 
-                //Fórmulas
+            //Fórmulas
     var j = c*i/100*t   //juros
     var m = c+j         //montante
 
-
-    
-    var juros = document.getElementById('j')
-    var montante = document.getElementById('m')
+    var juros = document.getElementsByClassName('juros')[0];
+    var montante = document.getElementsByClassName('montante')[0];
 
     juros.innerHTML=`= ${j.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`
     montante.innerHTML=`= ${m.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`
-    
 }
             //Função limpar
 function limpar(){
     document.getElementById('capital').value="";
     document.getElementById('taxa').value="";
     document.getElementById('tempo').value="1";
-    var juros = document.getElementById('j')
+    var juros = document.getElementsByClassName('juros')[0];
     juros.innerHTML="= R$ 0,00"
-    var montante = document.getElementById('m')
+    var montante = document.getElementsByClassName('montante')[0];
     montante.innerHTML="= R$ 0,00"
 }
 
